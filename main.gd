b001eedb65de6e35dea02a0f68bbb29fae5b3a7c
Jan-W-M <marneus3633@gmail.com>
2024-04-24 02:09:03 +0200

    movement done

--- main.gd
+++ main.gd
@@ -8,7 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(Input.mouse_mode)
 	if Input.is_action_just_pressed("ui_cancel") and Input.mouse_mode == 2:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	elif Input.is_action_just_pressed("ui_cancel") and Input.mouse_mode == 0:
--- player.gd
+++ player.gd
@@ -5,26 +5,31 @@ var twist_input := 0.0
 var pitch_input := 0.0
 
 var speed = 2000
-var jump_speed = 10
-var dash_speed =50
+var jump_speed = 25
+var dash_speed = 40
 var dash_cooldown = 0
 var jump_cooldown = 0 #i should add a new mesh to check if on floor, this is stupid
 var is_on_floor = false
-
+var is_falling = false
+var cur_height
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
 
 func _ready() -> void:
-	pass
-	
+	gravity_scale = 2
+
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("changejumpheight"):
-		jump_speed += 5
+
 
 	var input := Vector3.ZERO
+	
 	input.x = Input.get_axis("left", "right")
 	input.z = Input.get_axis("forward", "back")
+	#if not is_on_floor:
+		#input.y = - 1
 	if Input.is_action_just_pressed("dash") and dash_cooldown == 0:
 		input *= dash_speed
 		dash_cooldown = 1
@@ -32,17 +37,20 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor:
 		input.y = jump_speed
 		jump_cooldown = 1
+		print("jump")
 		#$JumpTimer.start()
-	
+	if not is_on_floor:
+		input *= 1.2 
+		
+	#print(input)
 	apply_central_force(twist_pivot.basis * input * speed * delta)
-	
-	var aligned_force = twist_pivot.basis * input
+	cur_height = global_position.y
 	
 		
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
 	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
-		deg_to_rad(-30), 
+		deg_to_rad(-35), 
 		deg_to_rad(30)
 	)
 	twist_input = 0.0
